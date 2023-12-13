4ec070261978abde18e065b8eb9e11c36d0090a8
widyanbudi <widyanbudiarsyah24@gmail.com>
2023-12-13 10:38:41 +0700

    as

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'My Work timer',
+      title: 'My Work waktu',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blueGrey,
